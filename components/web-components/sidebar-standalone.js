cf1873c1e83015d02ad233e5427482460b4e0b5d
Miomania <40alley@gmail.com>
2025-10-06 13:25:36 +0300

    Force remove left border from mobile nav links
    - Add !important to border-left: none for all nav-link elements
    - Add !important to border-right: none for all nav-link elements
    - Ensure no borders appear on mobile menu items
    - Override any inherited border styles

--- components/web-components/sidebar-standalone.js
+++ components/web-components/sidebar-standalone.js
@@ -114,8 +114,8 @@ class SGSidebar extends HTMLElement {
                         display: flex;
                         flex-direction: column;
                         align-items: center;
-                        border-right: none;
-                        border-left: none;
+                        border-right: none !important;
+                        border-left: none !important;
                     }
 
                     .nav-link.active {
